a04b24460b991d518bd59385aeddf541fa0e956d
wecol <xunqiduan@qq.com>
2013-11-03 05:35:03 +0800

    20131103update

---
+++ DesignPattern/TempleteDesign.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    public abstract class CoffeeinBeverage
+    {
+        public void BoiltWater()
+        {
+            Console.WriteLine("Boiting water....");
+        }
+
+        public void PrepareRecipe()
+        {
+            BoiltWater();
+            Brew();
+            PourInCup();
+            if( WantCondiments())
+            {
+                AddCondiments();
+
+            }
+            
+        }
+
+        public virtual bool WantCondiments()
+        {
+            return true;
+        }
+
+        public abstract void AddCondiments();
+
+        private void PourInCup()
+        {
+            Console.WriteLine("Pouring water into cup....");
+        }
+
+        public abstract void Brew();
+    }
+
+    public class Tea:CoffeeinBeverage
+    {
+
+
+        public override void AddCondiments()
+        {
+            Console.WriteLine("add salt");
+        }
+
+        public override void Brew()
+        {
+            Console.WriteLine("add tea");
+        }
+
+        public override bool WantCondiments()
+        {
+            return false;
+        }
+    }
+
+    public class NoSaltTea : CoffeeinBeverage
+    {
+
+
+        public override void AddCondiments()
+        {
+            Console.WriteLine("add salt");
+        }
+
+        public override void Brew()
+        {
+            Console.WriteLine("add tea");
+        }
+
+        public override bool WantCondiments()
+        {
+            return true;
+        }
+    }
+    
+}
